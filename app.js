1af1bc50d633fdc6fda2bf9f469855b330ee3186
Mohd Imran <mohd78696321@gmail.com>
2021-09-28 19:19:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,24 +8,30 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
-  if (billAmount.value > 0) {
-    if (cashGiven.value >= billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
-      calculateChange(amountToBeReturned);
-    } else {
-      showMessage("Should I Call the Police?");
-    }
-  } else {
-    showMessage("Invalid Bill Amount");
-  }
+  if (billAmount.value && cashGiven.value) {
+    if (billAmount.value >= 0 && cashGiven.value >= 0) {
+      if (Number(billAmount.value) != Number(cashGiven.value)) {
+        if (Number(cashGiven.value) >= Number(billAmount.value)) {
+          const changeAmount =
+            Number(cashGiven.value) - Number(billAmount.value);
+          showMessage("Change to be returned is Rs." + changeAmount);
+          calculateChange(changeAmount);
+        } else {
+          showMessage(
+            "Should I call the police? or Do you wanna wash plates?."
+          );
+          calculateChange(0);
+        }
+      } else showMessage("No change needs to be returned");
+    } else showMessage("Entered amount is less than 0.");
+  } else showMessage("Enter value in both the fields!");
 });
 
-function calculateChange(amountToBeReturned) {
-  for (var i = 0; i < availableNotes.length; i++) {
-    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-
-    amountToBeReturned = amountToBeReturned % availableNotes[i];
-    noOfNotes[i].innerText = numberOfNotes;
+function calculateChange(amount) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const notes = Math.trunc(amount / availableNotes[i]);
+    amount = amount % availableNotes[i];
+    noOfNotes[i].innerText = notes;
   }
 }
 
